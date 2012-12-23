e570529e1fa149ea973837d4af13c8b1df2a23ff
matthiasbock <mail@matthiasbock.net>
2012-12-23 22:08:50 +0100

    parsing works

--- movie2k.py
+++ movie2k.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-import sys
+#import sys
 from httpclient import HttpClient
 from htmlparser import between
 from moviexml import *
 
-url = 'http://www.movie2k.to/tvshows-1417991-Doctor-Who.html'
-#sys.argv[1])
+def parsePage(url):
+	client = HttpClient()
+	client.GET(url)
+	print ' parsing...'
 
-client = HttpClient()
-client.GET(url)
-
-if 'movie2k.to' in url:
-	xml = MovieXML()
 	url = between( between(client.Page, 'Watch movie', 'IMDB Rating'), '<a target="_blank" href="', '"' )
 	series = between(client.Page, 'style="color:#000000;">', '<').strip().replace('\n', ' ')
 	season = between(client.Page, '>, Season ', ',').strip()
 	episode = between(client.Page, 'Episode ', '<').strip()
 
-	movie = Movie(series=series, season=season, episode=episode)
-	movie.append( Source(type='stream', url=url) )
-	xml.append(movie)
-	print str(xml)
+	global xml
 
+	_series = xml.getSeries(series)
+	_season = _series.getSeason(season)
+	_episode = _season.getEpisode(episode)
+	_source= _episode.getSource(url)
+	_source.update(type='stream')
+ 
+xml = MovieXML()
+print 'GET ...'
+parsePage('http://www.movie2k.to/tvshows-1417991-Doctor-Who.html')
+print 'GET ...'
+parsePage('http://www.movie2k.to/tvshows-1454111-Torchwood.html')
+print str(xml)
--- moviexml.py
+++ moviexml.py
@@ -3,46 +3,124 @@
 
 #class page:
 
+def indent(s):
+	result = ''
+	for line in s.split('\n'):
+		result += '\t'+line+'\n'
+	return result
+
 class Source:
-	def __init__(self, type=None, audio=None, subtitles=None, url=None, indent=2*'\t'):
+	def __init__(self, type=None, audio=None, subtitles=None, url=None):
 		self.type = type
 		self.audio = audio
 		self.subtitles = subtitles
 		self.url = url
-		self.indent = indent
 		
 	def __str__(self):
-		xml = self.indent+'<source '
-		xml += 'url="'+self.url+'" '
-		xml += '/>\n'
+		xml = '<source'
+		attr = {	'type':		self.type,
+				'audio':		self.audio,
+				'subtitles':	self.subtitles
+			}
+		for a in attr.keys():
+			if attr[a] != None:
+				xml += ' '+a+'="'+attr[a]+'"'
+		xml += '>'+self.url+'</source>'
 		return xml
 
-class Movie:
-	def __init__(self, type=None, series=None, season=None, episode=None, title=None, indent=1*'\t'):
-		self.type = type
-		self.season = season
-		self.episode = episode
+class Episode:
+	def __init__(self, id=None, title=None):
+		self.id = id
 		self.title = title
-		self.indent = indent
 		self.children = []
 
 	def append(self, child):
 		self.children.append(child)
-	
+
 	def __str__(self):
-		xml = self.indent+'<movie '
-		attr = {	'type': 		self.type,
-				'season':		self.season,
-				'episode':	self.episode,
+		xml = '<episode'
+		attr = {	'id':		self.id,
 				'title':		self.title
 			}
 		for a in attr.keys():
 			if attr[a] != None:
-				xml += a+'="'+attr[a]+'" '
+				xml += ' '+a+'="'+attr[a]+'"'
 		xml += '>\n'
 		for child in self.children:
-			xml += str(child)
-		xml += self.indent+'</movie>\n'
+			xml += indent(str(child))
+		xml += '</episode>'
+		return xml
+
+class Season:
+	def __init__(self, id=None):
+		self.id = id
+		self.children = []
+
+	def append(self, child):
+		self.children.append(child)
+
+	def getEpisode(self, id):
+		episode = None
+
+		# find a child, that is a Series with the given title
+		for child in self.children:
+			if type(child) == Season:
+				if child.id == id:
+					episode = child
+					continue
+
+		# not found: create
+		if episode is None:
+			episode = Episode(id=id)
+			self.children.append(episode)
+		return episode
+
+	def __str__(self):
+		xml = '<season'
+		attr = {	'id':		self.id	}
+		for a in attr.keys():
+			if attr[a] != None:
+				xml += ' '+a+'="'+attr[a]+'"'
+		xml += '>\n'
+		for child in self.children:
+			xml += indent(str(child))
+		xml += '</season>'
+		return xml
+
+class Series:
+	def __init__(self, title=None):
+		self.title = title
+		self.children = []
+
+	def append(self, child):
+		self.children.append(child)
+
+	def getSeason(self, id):
+		season = None
+
+		# find a child, that is a Series with the given title
+		for child in self.children:
+			if type(child) == Season:
+				if child.id == id:
+					season = child
+					continue
+
+		# not found: create
+		if season is None:
+			season = Season(id=id)
+			self.children.append(season)
+		return season
+		
+	def __str__(self):
+		xml = '<series'
+		attr = {	'title':		self.title	}
+		for a in attr.keys():
+			if attr[a] != None:
+				xml += ' '+a+'="'+attr[a]+'"'
+		xml += '>\n'
+		for child in self.children:
+			xml += indent(str(child))
+		xml += '</series>'
 		return xml
 
 class MovieXML:
@@ -52,10 +130,26 @@ class MovieXML:
 	def append(self, child):
 		self.children.append(child)
 
+	def getSeries(self, title):
+		series = None
+
+		# find a child, that is a Series with the given title
+		for child in self.children:
+			if type(child) == Series:
+				if child.title == title:
+					series = child
+					continue
+
+		# not found: create
+		if series is None:
+			series = Series(title=title)
+			self.children.append(series)
+		return series
+
 	def __str__(self):
 		xml = '<xml>\n'
 		for child in self.children:
-			xml += str(child)
+			xml += indent(str(child))
 		xml += '</xml>'
 		return xml
 
